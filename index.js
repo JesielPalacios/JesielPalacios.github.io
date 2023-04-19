54012bcab57a0bd4f65544212f618d6f3bae94f9
Jesiel Palacios <jopalacioss@unac.edu.co>
2023-04-19 18:58:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import react from './images/logos/logo-react.svg'
+import mui from './images/logos/mui.png'
+import express from './images/logos/expressjs.webp'
+
+import propertysales from './images/works/propertysales.png'
+
+export { react, mui, propertysales, express }
